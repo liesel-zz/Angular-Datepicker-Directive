62d10637fda8b51216e4614342e3d05f98acfeed
Joel Backschat <joel@cafecomjava.com.br>
2014-09-02 13:10:17 -0300

    Atualização

--- js/DatePickerController.js
+++ js/DatePickerController.js
@@ -11,14 +11,14 @@ function DatePickerController($scope){
         return day;
     };
     $scope.dateChanged = function(date){
-    	if (date.getMonth() == 9) {
-    		var d1 				= new Date(2014,09,11);
-			var d2 				= new Date(2014,09,12);
-			var d3 				= new Date(2014,09,17);
-			$scope.startRange 	= new Date(2014,09,11);
-			$scope.rangeEnd 	= new Date(2014,09,19);
+    	var d = date.getMonth();
+    	var y = date.getFullYear();
+    		var d1 				= new Date(y,d,11);
+			var d2 				= new Date(y,d,12);
+			var d3 				= new Date(y,d,17);
+			$scope.startRange 	= new Date(y,d,11);
+			$scope.rangeEnd 	= new Date(y,d,19);
 			$scope.windows 		= [d1,d2, d3];
     		console.log(date);
-    	};
     }
 }
